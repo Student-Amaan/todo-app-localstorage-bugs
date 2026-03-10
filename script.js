7cc37eeac456cbfeee49514ed09e5716424b55c0
waxir <danishdarman06@gmail.com>
2026-03-10 14:29:01 +0500

    localStorage bugs is commited

---
+++ script.js
@@ -0,0 +1,7 @@
+const inputEl = document.querySelector("#input-field");
+const addBtn = document.querySelector("#add-task-btn");
+const addtask = document.querySelector("add-list");
+
+let tasks = []
+
+document

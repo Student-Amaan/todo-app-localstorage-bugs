441425ae78c8c6c452a62903588d3a74ec22550c
waxir <danishdarman06@gmail.com>
2026-03-11 23:10:29 +0500

    todo local storage is commited

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-const inputEl = document.querySelector("#input-field");
+const inputField = document.querySelector("#input-field");
 const addBtn = document.querySelector("#add-task-btn");
 const addtask = document.querySelector("add-list");
 
-let tasks = []
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-document
+tasks.forEach((task) => randerTask(task))
+
+addBtn.addEventListener("click", () => {
+  const inputText = inputField.value.trim();
+  if (inputText === "") return;
+
+  const newTask = {
+    id: Date.now(),
+    
+    Text: inputText,
+    completed: false,
+  };
+
+  tasks.push(newTask);
+  setItems()
+  inputField.value = "";
+  console.log(tasks);
+});
+
+function randerTask(task){
+    console.log(task)
+}
+
+function setItems(){
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+}

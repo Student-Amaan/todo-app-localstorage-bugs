aaaea09289ba84928b183da6fd0b8b0e52022b05
waxir <danishdarman06@gmail.com>
2026-03-11 23:35:45 +0500

    rander task function is commited

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
-const inputField = document.querySelector("#input-field");
-const addBtn = document.querySelector("#add-task-btn");
-const addtask = document.querySelector("add-list");
+document.addEventListener("DOMContentLoaded", () => {
+  const inputField = document.querySelector("#input-field");
+  const addBtn = document.querySelector("#add-task-btn");
+  const addtask = document.querySelector("#add-list");
 
-let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-tasks.forEach((task) => randerTask(task))
+  tasks.forEach((task) => randerTask(task));
 
-addBtn.addEventListener("click", () => {
-  const inputText = inputField.value.trim();
-  if (inputText === "") return;
+  addBtn.addEventListener("click", () => {
+    const inputText = inputField.value.trim();
+    if (inputText === "") return;
 
-  const newTask = {
-    id: Date.now(),
-    
-    Text: inputText,
-    completed: false,
-  };
+    const newTask = {
+      id: Date.now(),
 
-  tasks.push(newTask);
-  setItems()
-  inputField.value = "";
-  console.log(tasks);
+      Text: inputText,
+      completed: false,
+    };
+
+    tasks.push(newTask);
+    setItems();
+    inputField.value = "";
+    console.log(tasks);
+  });
+
+  function randerTask(task) {
+    const li = document.createElement("li");
+    li.setAttribute("data-id", task.id);
+    li.innerHTML = `<span>${task.Text}</span>
+      <button>delete</button>
+      `;
+    addtask.appendChild(li);
+  }
+
+  function setItems() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
 });
-
-function randerTask(task){
-    console.log(task)
-}
-
-function setItems(){
-    localStorage.setItem("tasks", JSON.stringify(tasks))
-}
